fa2562cc566de77bbd65e0ce557ee45adbec92e8
Paul Matchett <p.matchett80@gmail.com>
2017-06-07 21:08:31 +0100

    routing added to project

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -8,13 +9,34 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit{
 
-  @Input() recipe: Recipe; 
+  recipe: Recipe; 
+  id: number;
 
-  constructor(private recipeService: RecipeService){}
+  constructor(
+      private recipeService: RecipeService,
+      private route: ActivatedRoute,
+      private router: Router
+  ){}
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      )
+  }
 
   onAddToShoppongList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    //this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
+
 }
