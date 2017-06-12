e590b2a06a0d6b0e2b1aac0d38a0b801ad880d42
Paul Matchett <p.matchett80@gmail.com>
2017-06-12 21:25:10 +0100

    optimised services with rxjs subscriptions

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,9 @@
-import { Injectable, EventEmitter } from '@angular/core';
-
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs/Rx';
 
-@Injectable()
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Bananas', 6)
@@ -17,12 +15,12 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
 }
