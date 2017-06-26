3d9903adb301258450640878d930e5dc4cd5c6c5
Paul Matchett <p.matchett80@gmail.com>
2017-06-26 20:11:58 +0100

    added form logig to shopping list using form template method

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,11 @@
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from './../shared/ingredient.model';
 import { Subject } from 'rxjs/Rx';
 
 export class ShoppingListService {
 
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+  
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Bananas', 6)
@@ -13,6 +15,10 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
@@ -20,6 +26,16 @@ export class ShoppingListService {
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngradient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngradient(index: number, newIngradient: Ingredient) {
+    this.ingredients[index] = newIngradient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
